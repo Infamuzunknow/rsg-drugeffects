7424ef6315939e55512db17ac5615af46154a503
Infamuzunknow <113715560+Infamuzunknow@users.noreply.github.com>
2023-02-01 07:50:32 -0800

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,23 +1,35 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
 
 RSGCore.Functions.CreateUseableItem("joint", function(source, item)
-    TriggerClientEvent('rsg-drugeffects:JointAnim', source, item.name)
+    local Player = RSGCore.Functions.GetPlayer(source)
+    if Player.Functions.RemoveItem(item.name, 1, item.slot) then
+        TriggerClientEvent('rsg-drugeffects:JointAnim', source, item.name)
 	TriggerClientEvent("rsg-drugeffects:SetGoldenHealth", source, item.name)
+    end    
 end)
 
 RSGCore.Functions.CreateUseableItem("opium", function(source, item)
-    TriggerClientEvent('rsg-drugeffects:Opium', source, item.name)
+    local Player = RSGCore.Functions.GetPlayer(source)
+    if Player.Functions.RemoveItem(item.name, 1, item.slot) then
+        TriggerClientEvent('rsg-drugeffects:Opium', source, item.name)
 	TriggerClientEvent("rsg-drugeffects:SetGoldenStamina", source, item.name)
+    end
 end)
 
 RSGCore.Functions.CreateUseableItem("shrooms", function(source, item)
-    TriggerClientEvent('rsg-drugeffects:Mushroom', source, item.name)
+    local Player = RSGCore.Functions.GetPlayer(source)
+    if Player.Functions.RemoveItem(item.name, 1, item.slot) then
+        TriggerClientEvent('rsg-drugeffects:Mushroom', source, item.name)
 	TriggerClientEvent("rsg-drugeffects:SetGoldenDeadeye", source, item.name)
+    end
 end)
 
 RSGCore.Functions.CreateUseableItem("holy_trinity", function(source, item)
-    TriggerClientEvent('Consumables:client:Drink', source, item.name)
+    local Player = RSGCore.Functions.GetPlayer(source)
+    if Player.Functions.RemoveItem(item.name, 1, item.slot) then
+        TriggerClientEvent('Consumables:client:Drink', source, item.name)
 	TriggerClientEvent("rsg-drugeffects:SetGoldenTrinity", source, item.name)
+    end
 end)
 
 
